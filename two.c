a3a3d822bd92342b5480bc5ab751ab1eb9582403
shanthiboopathi <30317727+shanthiboopathi@users.noreply.github.com>
2017-07-24 14:29:14 +0530

    Create two.c

---
+++ two.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define   SIZE  12
+ int main()
+{
+int a[SIZE] = {1,3,5,4,7,2,99,16,45,67,89,45};
+int i, total = 0;
+for(i = 0; i <= (SIZE-1); i++)
+{
+printf("\n a[%d]= %d", i, a[i]);
+total += a[i];
+}
+printf("\nThe sum of the array elements is %d\n", total);
+return  0;
+}
+ 
